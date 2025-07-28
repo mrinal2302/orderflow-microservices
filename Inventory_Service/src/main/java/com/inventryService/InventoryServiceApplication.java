64d737b4074f04aa11059c7f1e00da2c0f11e13c
Vishal <vishalkumarrambo@gmail.com>
2025-07-28 15:43:27 +0530

    Changes done for InventoryService Setup

--- Inventory_Service/src/main/java/com/inventryService/InventoryServiceApplication.java
+++ Inventory_Service/src/main/java/com/inventryService/InventoryServiceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+public class InventoryServiceApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(OrderServiceApplication.class, args);
+        SpringApplication.run(InventoryServiceApplication.class, args);
 
     }
 
