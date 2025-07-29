002e6d7abc1f88564605915139bf1585ebc11994
vishalkumarsinha02 <vishalkumarrambo@gmail.com>
2025-07-29 13:50:18 +0530

    Ofs s1 inventory (#6)
    * Changes done for InventoryService Setup
    
    * Changes done for InventoryService Setup
    
    * Changes done for InventoryService Setup
    
    * Changes done for InventoryService Setup
    
    * Changes done for InventoryService Setup
    
    * Changes done for InventoryService Setup
    
    * Changes done for InventoryService Setup
    
    * Changes done for InventoryService Setup

---
+++ Inventory_Service/src/main/java/com/inventryService/repository/InventoryRepository.java
@@ -0,0 +1,14 @@
+package com.inventryService.repository;
+
+import com.inventryService.entity.InventoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
+
+    List<InventoryEntity> findByProductNameStartingWithIgnoringCase(String productName);
+
+}
